2f4884c5331d1d46c0932547231d017addd5fadc
Alexandre Roulois <alexandre.roulois@linguist.univ-paris-diderot.fr>
2019-10-13 10:04:45 +0200

    Comments translated in english

--- LLF-connector.py
+++ LLF-connector.py
@@ -1,44 +1,48 @@
 #/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-# Import des modules
+# Modules import
 import json, os.path, platform
 
-# Chemin vers le fichier des moteurs, pour un utilisateur Mac ou Linux
-path = os.path.expanduser('~') + '/Zotero/locate/engines.json' if (platform.system() in ['Darwin', 'Linux']) else exit()
+if __name__ == "__main__":
 
-# Lecture séquentielle du fichier des moteurs Zotero
-with open(path) as src:
-	txt = temp = ""
-	while True:
-		temp = src.read(10)
-		if temp == "":
-			break
-		else:
-			txt += temp
+    # Path to Zotero engines (mac & linux)
+    path = os.path.expanduser('~') + '/Zotero/locate/engines.json' if (platform.system() in ['Darwin', 'Linux']) else exit()
 
-# Lecture des données au format JSON
-data = json.loads(txt)
+    # Reading the file
+    with open(path) as src:
+        txt = temp = ""
+        while True:
+            temp = src.read(10)
+            if temp == "":
+                break
+            else:
+                txt += temp
 
-# Arrêt du programme si le moteur du LLF existe déjà
-[exit() if (engine['alias'] == 'LLF') else True for engine in data]
+    # Data in JSON
+    data = json.loads(txt)
 
-# Configuration du moteur du LLF
-LLFengine = {
-	"name": "LLF : quelles sont les publications de l’auteur ?",
-	"alias": "LLF",
-	"icon": "http://www.llf.cnrs.fr/sites/llf.cnrs.fr/files/favicon.png",
-	"_urlTemplate": "http://www.llf.cnrs.fr/biblio/author/{rft:aulast?}",
-	"description": "Connecteur Zotero des publications du LLF",
-	"hidden": False,
-	"_urlParams": [],
-	"_urlNamespaces": {"rft":"info:ofi/fmt:kev:mtx:journal"}
-}
-# Ajouter le moteur à la fin de la liste
-data.append(LLFengine)
-# Renverser l'ordre de la liste pour que le moteur du LLF apparaisse en 1er !
-data.reverse()
+    # Exit if engine already installed
+    [exit() if (engine['alias'] == 'LLF') else True for engine in data]
 
-# Écriture de la liste
-with open(path, 'w') as dest:
-	json.dump(data, dest, separators=(',', ':'), indent=4)
+    # Engine configuration
+    LLFengine = {
+        "name": "LLF : quelles sont les publications de l’auteur ?",
+        "alias": "LLF",
+        "icon": "http://www.llf.cnrs.fr/sites/llf.cnrs.fr/files/favicon.png",
+        "_urlTemplate": "http://www.llf.cnrs.fr/biblio/author/{rft:aulast?}",
+        "description": "Connecteur Zotero des publications du LLF",
+        "hidden": False,
+        "_urlParams": [],
+        "_urlNamespaces": {"rft":"info:ofi/fmt:kev:mtx:journal"}
+    }
+
+    # Adding the engine to the file
+    data.append(LLFengine)
+
+    # Reverse order
+    data.reverse()
+
+    # Saving the configuration file of the engines
+    with open(path, 'w') as dest:
+        json.dump(data, dest, separators=(',', ':'), indent=4)
